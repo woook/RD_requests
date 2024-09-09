8c0b85d904bf304c79803a7c36cdb53edf67522b
rklocke <rebeccalocke4@msn.com>
2024-09-09 10:59:55 +0100

    Update writing of files, add function docstrings and update README

--- EBH-3050/README.md
+++ EBH-3050/README.md
@@ -8,20 +8,26 @@ e.g. R208, 500 tests, 200 with no workbook, 300 with a workbook"
 ### Inputs
 The script `get_workbook_release_per_clin_ind.py` takes inputs:
 - `--start_date (str)`: The date the new filtering was introduced (format %Y-%m-%d, e.g. 2024-04-09).
+- `--end_date (str)`: The date of the request to filter projects before (format %Y-%m-%d, e.g. 2024-09-04).
 - `--process_change (str)`: The date the CNV release process change was introduced, where we no longer release CNV workbooks for samples with no CNVs (format %Y-%m-%d, e.g. 2024-07-10).
 - `--ignore_files (file)`: A txt file with any files to ignore (one DNAnexus file ID per line)
 - `--outfile_name (str)`: The name of the output file.
 
+### Running
+Example command:
+`python get_workbook_release_per_clin_ind.py --start_date 2024-04-09 --end_date 2024-09-04 --process_change 2024-07-10 --ignore_files files_to_ignore.txt --outfile_name EBH-3050.xlsx`
+
 ### How it works
 The script:
-- Searches for all 002 CEN/TWE DNAnexus projects since `--start_date`
+- Searches for all 002 CEN/TWE DNAnexus projects since `--start_date` and `--end_date`
 - Within the projects found above, finds all SNV (`*SNV*.xlsx`) workbooks and CNV (`*CNV*.xlsx`) workbooks
 - For each of these workbooks, gets the DNAnexus 'details' which contains the clinical indication and the number of variants contained within the report
 - Writes this information to a Pandas dataframe, removes any files we've told it to ignore via `--ignore_files`
 - Merges all reports found for one sample and clinical indication into one row
 - Adds columns `SNV_report_released` and `CNV_report_released` to indicate whether a report was released (taking into account `--process_change`) and based on this, creates another column `any_report_released` if at least one of these columns are 'Yes'
-- Groups by `clinical_indication` and counts how many samples for each clinical indication and within this, how many with no SNV or CNV report released (`no_workbook_released`) and how many had a SNV or CNV report released (or both)
-- Writes the final grouped dataframe to an Excel named by `--outfile_name`.
+- Groups by `clinical_indication` and counts how many samples for each clinical indication and within this, how many with no SNV or CNV report released (`no_workbook_released`) and how many had a SNV or CNV report released (or both) (`workbook_released`).
+- Also groups by `clinical_indication` and counts how many samples for each clinical indication and within this, how many had no variants (SNV or CNV) and how many had variants.
 
 ### Output
-The output of the script is an Excel file with one sheet (`by_workbook_release`) showing clinical indications, total samples and within this, number with a workbook released / not released. There is also another sheet (`by_variants`) which has a similar breakdown but showing clinical indications, total samples and within this, number with any variants / no variants.
+The output of the script is an Excel file named by `--outfile_name` with one sheet (`by_workbook_release`) showing clinical indications, total samples and within this, number with a workbook released / not released. There is also another sheet (`by_variants`) which has a similar breakdown but showing clinical indications, total samples and within this, number with any variants / no variants.
+There is also an Excel file outputted containing the raw ungrouped data.
--- EBH-3050/get_workbook_release_per_clin_ind.py
+++ EBH-3050/get_workbook_release_per_clin_ind.py
@@ -31,6 +31,17 @@ def parse_args() -> argparse.Namespace:
     )
 
     parser.add_argument(
+        '-e',
+        '--end_date',
+        required=True,
+        type=str,
+        help=(
+            "Date to filter projects created before in %y%m%d format e.g. "
+            "2024-09-04"
+        )
+    )
+
+    parser.add_argument(
         '-p',
         '--process_change',
         required=True,
@@ -59,14 +70,16 @@ def parse_args() -> argparse.Namespace:
 
     return parser.parse_args()
 
-def find_dx_projects(start_date):
+def find_dx_projects(start_date, end_date):
     """
-    Find 002 CEN/TWE projects made after certain date
+    Find 002 CEN/TWE projects between certain dates
 
     Parameters
     ----------
     start_date : str
         date to filter projects created after e.g. '2024-04-09'
+    end_date : str
+        date to filter projects created before e.g. '2024-09-04'
 
     Returns
     -------
@@ -83,7 +96,8 @@ def find_dx_projects(start_date):
                 "name": True
             }
         },
-        created_after=start_date
+        created_after=start_date,
+        created_before=end_date
     ))
 
     return projects
@@ -92,8 +106,8 @@ def find_reports(project_id, report_type):
     """
     Find xlsx reports in the DX project given. This searches the /output
     folder as one project (project-GkX7Qf84ByB0F0bpzxy4FZ57) has a folder
-    where CNV was re-run on request with a failed sample and put in another
-    folder
+    where CNV was re-run on request with a failed sample and put in a
+    different folder
 
     Parameters
     ----------
@@ -122,9 +136,63 @@ def find_reports(project_id, report_type):
 
     return reports
 
+
+def get_reports(projects_002):
+    """
+    Get all reports in all 002 projects and put them in a big list
+
+    Parameters
+    ----------
+    projects_002 : list
+        list of dicts, each with info about a DX project
+
+    Returns
+    -------
+    all_reports : list
+        list of dicts, each representing a SNV or CNV report
+    """
+        # Find SNV and then CNV reports in all of those projects and store
+    # as list of dictionaries, each with info about a report
+    all_reports = []
+    for project in projects_002:
+        print(project['describe']['name'])
+
+        # Find SNV reports in project and save info about them
+        snv_reports = find_reports(project['id'], 'SNV')
+        print(f"{len(snv_reports)} SNV reports found")
+        for snv_report in snv_reports:
+            sample_name = "-".join(
+                snv_report['describe']['name'].split("-", 2)[:2]
+            )
+            all_reports.append({
+                'run': project['describe']['name'],
+                'sample': sample_name,
+                'snv_file_id': snv_report['id'],
+                'type': 'SNV',
+            })
+
+        # Find CNV reports in project and save info about them
+        cnv_reports = find_reports(project['id'], 'CNV')
+        print(f"{len(cnv_reports)} CNV reports found")
+        for cnv_report in cnv_reports:
+            sample_name = "-".join(
+                cnv_report['describe']['name'].split("-", 2)[:2]
+            )
+            all_reports.append({
+                'run': project['describe']['name'],
+                'sample': sample_name,
+                'cnv_file_id': cnv_report['id'],
+                'type': 'CNV',
+            })
+
+        print("")
+
+    return all_reports
+
+
 def get_details_in_parallel(list_of_files) -> list:
     """
-    Call dx.dxFile().get_details() in parallel for given list of files.
+    Call dx.dxFile().get_details() in parallel for given list of files
 
     Parameters
     ----------
@@ -193,77 +261,60 @@ def get_details_in_parallel(list_of_files) -> list:
     return results
 
 
-def main():
-    """Main function"""
-    args = parse_args()
-    # Find all 002 Dias projects since new filtering introduced
-    # start_date = '2024-04-10'
-    projects_002 = find_dx_projects(args.start_date)
-    print(f"Found {len(projects_002)} 002 projects since {args.start_date}")
+def remove_ignore_files(sample_df, ignore_txt_file):
+    """
+    Remove any files (by ID) if they are in the input txt file containing
+    files to ignore
 
-    # Find SNV and then CNV reports in all of those projects and store
-    # as list of dictionaries, each with info about a report
-    all_reports = []
-    for project in projects_002:
-        print(project['describe']['name'])
+    Parameters
+    ----------
+    sample_df : pd.DataFrame
+        dataframe with all SNV/CNV reports found
+    ignore_txt_file : str
+        name of txt file with file IDs to ignore
 
-        # Find SNV reports in project and save info about them
-        snv_reports = find_reports(project['id'], 'SNV')
-        print(f"{len(snv_reports)} SNV reports found")
-        for snv_report in snv_reports:
-            sample_name = "-".join(
-                snv_report['describe']['name'].split("-", 2)[:2]
-            )
-            all_reports.append({
-                'run': project['describe']['name'],
-                'sample': sample_name,
-                'snv_file_id': snv_report['id'],
-                'type': 'SNV',
-            })
-
-        # Find CNV reports in project and save info about them
-        cnv_reports = find_reports(project['id'], 'CNV')
-        print(f"{len(cnv_reports)} CNV reports found")
-        for cnv_report in cnv_reports:
-            sample_name = "-".join(
-                cnv_report['describe']['name'].split("-", 2)[:2]
-            )
-            all_reports.append({
-                'run': project['describe']['name'],
-                'sample': sample_name,
-                'cnv_file_id': cnv_report['id'],
-                'type': 'CNV',
-            })
-
-        print("")
-
-    # Add in details of included variants and clinical indication for all
-    # reports in parallel
-    reports_with_details = get_details_in_parallel(all_reports)
-
-    # Make df of all reports (multiple rows per sample, one for each report)
-    variants_df = pd.DataFrame(reports_with_details)
-
+    Returns
+    -------
+    variants_df_all_ignored_removed : df
+        df with rows removed if the file ID is in the ignore list
+    """
     # Ignore certain files as they're ad hoc requests (e.g. Sticklers) or
     # reports run for testing of dias_reports_bulk_reanalysis
-    with open(args.ignore_files, mode ='r', encoding='utf8') as file_with_ids:
+    with open(ignore_txt_file, mode ='r', encoding='utf8') as file_with_ids:
         file_ignore_list = [line.rstrip('\n') for line in file_with_ids]
 
     files_ignore = '\n\t'.join([file for file in file_ignore_list])
     print(f"Ignoring following files:\n\t{files_ignore}")
 
-    # Remove rows from df if the file ID is in the row
-    variants_df_snv_files_removed = variants_df[
-        ~variants_df.snv_file_id.isin(file_ignore_list)
+    # Remove rows from df if the file ID is in the SNV file ID column
+    variants_df_snv_files_removed = sample_df[
+        ~sample_df.snv_file_id.isin(file_ignore_list)
     ]
+    # Remove rows from df if the file ID is in the CNV file ID column
     variants_df_all_ignored_removed = variants_df_snv_files_removed[
         ~variants_df_snv_files_removed.cnv_file_id.isin(file_ignore_list)
     ]
 
+    return variants_df_all_ignored_removed
 
+
+def group_by_test_and_add_run_date(reports_df):
+    """
+    Group by run, sample and clinical indication
+
+    Parameters
+    ----------
+    reports_df : pd.DataFrame
+        dataframe with one row per report found
+
+    Returns
+    -------
+    grouped_df : pd.DataFrame
+        dataframe with one row per sample with info on SNV/CNV reports found
+    """
     # Group by run, sample and clinical indication so we end up with one row
     # per sample (and can see which have SNV+CNV or just SNV)
-    grouped_df = variants_df_all_ignored_removed.groupby(
+    grouped_df = reports_df.groupby(
         ['run', 'sample', 'clinical_indication']
     ).agg({
         'type': lambda x: ','.join(x),
@@ -273,14 +324,36 @@ def main():
         'cnv_included_variants': 'first'
     }).reset_index()
 
-    # Add run date so we can work out whether CNV report was released due to
-    # artemis update, and convert to date type
+    # Add run date so later we can work out whether CNV report was released
+    # due to artemis update, and convert to date type
     grouped_df['run_date'] = grouped_df['run'].apply(lambda s:s.split('_')[1])
     grouped_df['run_date'] = pd.to_datetime(
         grouped_df['run_date'], format="%y%m%d"
     ).dt.date
 
-    # Add column to say whether SNV report was released (if > 0 variants)
+    return grouped_df
+
+
+def determine_whether_any_report_released(grouped_df, process_change_date):
+    """
+    Create new columns with info on whether any report was released
+
+    Parameters
+    ----------
+    grouped_df : pd.DataFrame
+        df containing info on each test and variants found
+    process_change_date : str
+        date that the artemis release process changed so CNV reports were
+        no longer released if had 0 variants
+
+    Returns
+    -------
+    grouped_df : pd.DataFrame
+        dataframe with extra columns 'SNV_report_released',
+        'CNV_report_released' and 'any_report_released'
+    """
+    # Add column to say whether SNV report was released (yes if > 0 variants
+    # otherwise no)
     grouped_df['SNV_report_released'] = np.where(
         grouped_df['snv_included_variants'] > 0,
         'Yes', 'No'
@@ -289,7 +362,7 @@ def main():
     # Set conditions for whether CNV report was released based on when the run
     # was released and whether there were CNVs identified
     cnv_process_change = datetime.datetime.strptime(
-        args.process_change, '%Y-%m-%d'
+        process_change_date, '%Y-%m-%d'
     ).date()
 
     conditions = [
@@ -316,12 +389,11 @@ def main():
     ]
 
     values = ["Yes", "No", "Yes", "No", "No"]
-
     # Add column to say whether CNV report was released
     grouped_df['CNV_report_released'] = np.select(conditions, values)
 
+
     # Add extra column to say whether any report was released for the sample
-    # based on conditions
     second_conditions = [
         (
             (grouped_df['SNV_report_released'] == 'Yes')
@@ -346,6 +418,27 @@ def main():
         second_conditions, second_values
     )
 
+    return grouped_df
+
+
+def group_by_report_release(grouped_df):
+    """
+    Group by clinical indication and count how many samples, how many had no
+    report released and how many had at least one report released
+
+    Parameters
+    ----------
+    grouped_df : pd.DataFrame
+        df with columns 'SNV_report_released', 'CNV_report_released' and
+        'any_report_released'
+
+    Returns
+    -------
+    sorted_any_workbook : pd.DataFrame
+        df with info on how many samples had no report released per clinical
+        indication
+    """
+
     # Group by clinical indication and count how many samples and of these,
     # how many had no workbook and how many had at least one workbook released
     grouped_any_workbook = grouped_df.groupby('clinical_indication').agg(
@@ -358,29 +451,191 @@ def main():
     sorted_any_workbook = grouped_any_workbook.sort_values(
         by=['total_samples'], ascending=False, ignore_index=True
     )
-    sorted_any_workbook['R_code'] = sorted_any_workbook[
-        'clinical_indication'
-    ].apply(lambda s:s.split('_')[0])
+
+    # # Add in column with short R code
+    # sorted_any_workbook['R_code'] = sorted_any_workbook[
+    #     'clinical_indication'
+    # ].apply(lambda s:s.split('_')[0])
+
     sorted_any_workbook = sorted_any_workbook[[
-        'R_code', 'clinical_indication', 'total_samples',
+        'clinical_indication', 'total_samples',
         'no_workbook_released', 'workbook_released'
     ]]
 
-    writer = pd.ExcelWriter(args.outfile_name)
-    sorted_any_workbook.to_excel(
-        writer, sheet_name='by_workbook_release', index=False
+    return sorted_any_workbook
+
+
+def group_by_any_variants(grouped_df):
+    """
+    Group by clinical indication and count how many had no variants in SNV or
+    CNV or both
+
+    Parameters
+    ----------
+    grouped_df : pd.DataFrame
+        df with columns 'snv_included_variants' and 'cnv_included_variants'
+
+    Returns
+    -------
+    sorted_any_variants : pd.DataFrame
+        df with info on how many samples had no variants in SNV, CNV or both
+        per clinical indication
+    """
+    grouped_df['variants_sum'] = grouped_df[
+        ['snv_included_variants', 'cnv_included_variants']
+    ].sum(axis=1)
+
+    # Group and count how many have 0 SNVs+CNVs and how many have >0 SNVs+CNVs
+    grouped_by_any_variants = grouped_df.groupby('clinical_indication').agg(
+        total_samples=('sample', 'size'),
+        no_variants=('variants_sum', lambda x: (x == 0).sum()),
+        any_variants=('variants_sum', lambda x: (x > 0).sum())
+    ).reset_index()
+
+    # Sort by total samples
+    sorted_any_variants = grouped_by_any_variants.sort_values(
+        by=['total_samples'], ascending=False, ignore_index=True
     )
-    for column in sorted_any_workbook:
+
+    sorted_any_variants = sorted_any_variants[[
+        'clinical_indication', 'total_samples', 'no_variants', 'any_variants'
+    ]]
+
+    return sorted_any_variants
+
+
+def write_out_intermediate_excel(grouped_df):
+    """
+    Write out the Excel with the raw data
+
+    Parameters
+    ----------
+    grouped_df : pd.DataFrame
+        pandas df with all info on each test
+    """
+    # Subset to remove file ID columns
+    grouped_df = grouped_df[[
+        'run', 'run_date', 'sample', 'clinical_indication', 'type',
+        'snv_included_variants', 'cnv_included_variants',
+        'SNV_report_released', 'CNV_report_released', 'any_report_released'
+    ]]
+
+    writer = pd.ExcelWriter('EBH-3050_raw_data.xlsx')
+    grouped_df.to_excel(
+        writer, sheet_name='all_data', index=False
+    )
+    # Automatically set column widths to fit content
+    for column in grouped_df:
         column_length = max(
-            sorted_any_workbook[column].astype(str).map(len).max(),
+            grouped_df[column].astype(str).map(len).max(),
             len(column)
         )
-        col_idx = sorted_any_workbook.columns.get_loc(column)
-        writer.sheets['by_workbook_release'].set_column(
+        col_idx = grouped_df.columns.get_loc(column)
+        writer.sheets['all_data'].set_column(
             col_idx, col_idx, column_length
         )
 
     writer.save()
+
+
+def write_out_final_excel(
+        by_workbook_df, by_variants_df, sheet_1, sheet_2, outfile_name
+    ):
+    """
+    Write out pandas dfs to sheets of Excel file
+
+    Parameters
+    ----------
+    by_workbook_df : pd.DataFrame
+        pandas df to write out
+    outfile_name : str
+        name of Excel file to write out
+    """
+    writer = pd.ExcelWriter(outfile_name)
+    by_workbook_df.to_excel(
+        writer, sheet_name=sheet_1, index=False
+    )
+    # Automatically set column widths to fit content
+    for column in by_workbook_df:
+        column_length = max(
+            by_workbook_df[column].astype(str).map(len).max(),
+            len(column)
+        )
+        col_idx = by_workbook_df.columns.get_loc(column)
+        writer.sheets[sheet_1].set_column(
+            col_idx, col_idx, column_length
+        )
+
+    by_variants_df.to_excel(
+        writer, sheet_name=sheet_2, index=False
+    )
+    for column in by_variants_df:
+        column_length = max(
+            by_variants_df[column].astype(str).map(len).max(),
+            len(column)
+        )
+        col_idx = by_variants_df.columns.get_loc(column)
+        writer.sheets[sheet_2].set_column(
+            col_idx, col_idx, column_length
+        )
+
+    writer.save()
+
+
+def main():
+    """Main function"""
+    args = parse_args()
+    # Find all 002 Dias projects since new filtering introduced
+    # start_date = '2024-04-10'
+    projects_002 = find_dx_projects(args.start_date, args.end_date)
+    print(f"Found {len(projects_002)} 002 projects since {args.start_date}")
+
+    # Find SNV and then CNV reports in all of those projects and store
+    # as list of dictionaries, each with info about a report
+    all_reports = get_reports(projects_002)
+
+    # Add in details of included variants and clinical indication for all
+    # reports in parallel
+    reports_with_details = get_details_in_parallel(all_reports)
+
+    # Make df of all reports (multiple rows per sample, one for each report)
+    variants_df = pd.DataFrame(reports_with_details)
+
+    # Ignore certain files as they're ad hoc requests (e.g. Sticklers) or
+    # reports run for testing of dias_reports_bulk_reanalysis
+    variants_df_all_ignored_removed = remove_ignore_files(
+        variants_df, args.ignore_files
+    )
+
+    # Group by run, sample and clinical indication so we end up with one row
+    # per sample (and can see which have SNV+CNV or just SNV)
+    grouped_df = group_by_test_and_add_run_date(
+        variants_df_all_ignored_removed
+    )
+
+    # Set conditions for whether CNV report was released based on when the run
+    # was released and whether there were CNVs identified
+    report_release_df = determine_whether_any_report_released(
+        grouped_df, args.process_change
+    )
+
+    # Write out intermediate Excel with all data
+    write_out_intermediate_excel(report_release_df)
+
+    # Create df grouped by clinical indication with counts of samples and
+    # how many with workbook released
+    sorted_any_workbook = group_by_report_release(report_release_df)
+
+    # Create df grouped by clinical indication with counts of samples and
+    # how many with any variants
+    sorted_any_variants = group_by_any_variants(report_release_df)
+
+    # Write out final Excel with two sheets, grouped by workbook release
+    # and grouped by any variants
+    write_out_final_excel(
+        sorted_any_workbook, sorted_any_variants, 'by_workbook_release',
+        'by_variants', args.outfile_name
+    )
 
 
 if __name__ == '__main__':
