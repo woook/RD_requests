d3c897fe19c2a021f6033ea0d9282bb93007ad12
growland2 <gregory.rowland@nhs.net>
2024-09-18 15:59:35 +0100

    fix

--- DI-773/qc_metrics_plotter.py
+++ DI-773/qc_metrics_plotter.py
@@ -262,7 +262,7 @@ def make_plot(
     passed_df = df[(df['QC_status'].str.lower() == 'pass') | (df['QC_status'].str.lower() == 'warning')].sort_values('run')
     failed_df = df[df['QC_status'].str.lower() == "fail"].sort_values('run')
     n_filtered_rows = len(passed_df) + len(failed_df)
-    assert n_filtered_rows != len(df), f"QC_Status column contains invalid values: {df['QC_status'].unique().tolist()}"
+    assert n_filtered_rows == len(df), f"QC_Status column contains invalid values: {df['QC_status'].unique().tolist()}"
 
     fig = px.box(
         passed_df,
